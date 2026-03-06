69d34217c78eacfbce0b78b333d14a211ab6266e
Han-GR <2411596211@qq.com>
2026-03-06 10:53:50 +0800

    初始化项目

---
+++ WanderFlowTests/WanderFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  WanderFlowTests.swift
+//  WanderFlowTests
+//
+//  Created by han han on 2026/3/6.
+//
+
+import Testing
+@testable import WanderFlow
+
+struct WanderFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
